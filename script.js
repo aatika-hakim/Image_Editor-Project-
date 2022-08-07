7885e895f40a0336d3e4fd765467f86d9bee5f00
Aatika Hakim <aatikahakim90@gmail.com>
2022-08-07 12:42:18 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,16 +1,64 @@
 const fileInput = document.querySelector(".file-input"),
-previewImgBtn = document.querySelector(".preview-img")
+filterSlider = document.querySelector(".slider input"),
+filterValue = document.querySelector(".filter-info .value"),
+filterName = document.querySelector(".filter-info .name"),
+filterOptions = document.querySelectorAll(".filters button"),
+previewImg = document.querySelector(".preview-img img"),
 chooseImgBtn = document.querySelector(".choose-img");
 
+// Default values
+let brightness = 100, saturation = 100, inversion = 0, grayscale = 0;
 
 const loadImage = () => {
     // getting user selectd file
-    let file = fileInput.file[0];
+    let file = fileInput.files[0];
     if(!file) return;
-    
+    previewImg.src = URL.createObjectURL(file);
+    previewImg.addEventListener("load", () => {
+        document.querySelector(".container").classList.remove("disable");
+    });
 }
 
-previewImgBtn.addEventListener("click")
+filterOptions.forEach(option => {
+    option.addEventListener("click", () => {
+        document.querySelector(".filters .active").classList.remove("active");
+        option.classList.add("active");
+        filterName.innerText = option.innerText;
+
+        if(option.id === "brightness"){
+            filterSlider
+            filterSlider.value = brightness;
+            filterValue.innerText = `${brightness}%`;
+        }else if(option.id === "saturation"){
+            filterSlider.value = saturation;
+            filterValue.innerText = `${saturation}%`;
+        }else if(option.id === "inversion"){
+            filterSlider.value = inversion;
+            filterValue.innerText = `${inversion}%`;
+        }else{
+            filterSlider.value = grayscale;
+            filterValue.innerText = `${grayscale}%`;
+        }
+    });
+});
+
+const updateFilter = () => {
+    filterValue.innerText = `${filterSlider.value}%`;
+    const selectedFilter = document.querySelector(".filter .active");
+
+    if(selectedFilter === "brightness"){
+        brightness = filterSlider.value;
+    }else if(selectedFilter === "saturation"){
+        saturation = filterSlider.value;
+    }else if(selectedFilter === "inversion"){
+        inversion = filterSlider.value;
+    }else{
+        grayscale= filterSlider.value;
+    }
+}
+
 
 fileInput.addEventListener("change",loadImage);
+filterSlider.addEventListener("input", updateFilter);
 chooseImgBtn.addEventListener("click",() => fileInput.click());
+
