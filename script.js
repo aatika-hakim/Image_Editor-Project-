3671dd62157206ae9cda65e7ed14da6fb30bdbba
Aatika Hakim <aatikahakim90@gmail.com>
2022-08-07 08:27:31 -0400

    image-Editor

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fileInput = document.querySelector(".file-input"),
+previewImgBtn = document.querySelector(".preview-img")
 chooseImgBtn = document.querySelector(".choose-img");
 
 
@@ -6,8 +7,10 @@ const loadImage = () => {
     // getting user selectd file
     let file = fileInput.file[0];
     if(!file) return;
-    console.log(file);
+    
 }
+
+previewImgBtn.addEventListener("click")
 
 fileInput.addEventListener("change",loadImage);
 chooseImgBtn.addEventListener("click",() => fileInput.click());
